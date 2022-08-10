7e03448189e4ef5d1ee224b7aa52e2f526acb366
daminakov <dmitriy.minakov@quantori.com>
2022-08-10 13:36:42 +0400

    Tests for homework. Vowels, spaces, punctuation. Blank request. Check buttons location

--- tests/test_vowels_as_default_state.py
+++ tests/test_vowels_as_default_state.py
@@ -1,12 +1,18 @@
-'''Tests for home works with Pylint Flake8'''
+"""Tests for homework.
+
+Checked with Pylint and Flake8 (IMHO Pylint more useful).
+"""
 from time import sleep
 from selenium import webdriver
 
-from selenium.webdriver.common.by import By
+import pyautogui
+
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
-chrome_driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+chrome_driver = webdriver.Chrome\
+    (service=Service(ChromeDriverManager().install()))
 VOWELS = {'а', 'е', 'ё', 'и', 'о', 'у', 'ы', 'э', 'ю', 'я'}
 SPACE = {' '}
 PUNTCUATION = {'.', ',', '-', '!', '?', ':', ';'}
@@ -14,9 +20,21 @@ PUNTCUATION = {'.', ',', '-', '!', '?', ':', ';'}
 chrome_driver.get('https://rioran.github.io/ru_vowels_filter/main.html')
 chrome_driver.maximize_window()
 
+BUTTON_VOWELS = chrome_driver.find_element \
+        (By.XPATH, "//button[contains(.,\'Оставить только гласные\')]")
+BUTTON_HIGHLIGHT =chrome_driver.find_element\
+        (By.XPATH, "//button[contains(.,\'Выделить результат\')]")
+BUTTON_SPACES = chrome_driver.find_element\
+        (By.XPATH, "//button[contains(.,\'Ну и ещё пробелы\')]")
+BUTTON_PUNCTUATION = chrome_driver.find_element\
+        (By.XPATH, "//button[contains(.,\'Оставить ещё и .,-!?\')]")
+
 
 def convert_text_into_set(text):
-    '''This function deploy text into list and change it into set'''
+    """Cycle for text.
+
+    This function deploy text into list and change it into set.
+    """
     char_list = []
     for char in text:
         if char not in char_list:
@@ -26,14 +44,17 @@ def convert_text_into_set(text):
 
 
 def test_vowels_with_default_text():
-    '''This test checks if on page present buttons which filtrate in default text only vowels'''
+    """TEST.
+
+    This test checks if on page present buttons which
+    filtrate in default text only vowels.
+    """
     text_input = chrome_driver.find_element('id', 'text_input').text
     set_input = convert_text_into_set(text_input)
     match_input = set_input.intersection(VOWELS)
     sleep(1)
-    chrome_driver.find_element \
-        (By.XPATH, "//button[contains(.,\'Оставить только гласные\')]").click()
-    chrome_driver.find_element(By.XPATH, "//button[contains(.,\'Выделить результат\')]").click()
+    BUTTON_VOWELS.click()
+    BUTTON_HIGHLIGHT.click()
     sleep(1)
     output_text = chrome_driver.find_element('id', 'text_output').text
     output_text = output_text.replace('\n', '')
@@ -43,15 +64,18 @@ def test_vowels_with_default_text():
 
 
 def test_vowels_space_with_default_text():
-    '''This test checks if on page present buttons which filtrate in default text only vowels
-    and spaces'''
+    """TEST.
+
+    This test checks if on page present buttons which
+    filtrate in default text only vowels and spaces.
+    """
     vowels_space = VOWELS.union(SPACE)
     text_input = chrome_driver.find_element('id', 'text_input').text
     set_input = convert_text_into_set(text_input)
     match_input = set_input.intersection(vowels_space)
     sleep(1)
-    chrome_driver.find_element(By.XPATH, "//button[contains(.,\'Ну и ещё пробелы\')]").click()
-    chrome_driver.find_element(By.XPATH, "//button[contains(.,\'Выделить результат\')]").click()
+    BUTTON_SPACES.click()
+    BUTTON_HIGHLIGHT.click()
     sleep(1)
     output_text = chrome_driver.find_element('id', 'text_output').text
     output_text = output_text.replace('\n', '')
@@ -61,15 +85,18 @@ def test_vowels_space_with_default_text():
 
 
 def test_vowels_space_punctuation_with_default_text():
-    '''This test checks if on page present buttons which filtrate in default text only
-    vowels, spaces and punctuation'''
+    """TEST.
+
+    This test checks if on page present buttons which
+    filtrate in default text only vowels, spaces and punctuation.
+    """
     vowels_space_punct = (VOWELS.union(SPACE)).union(PUNTCUATION)
     text_input = chrome_driver.find_element('id', 'text_input').text
     set_input = convert_text_into_set(text_input)
     match_input = set_input.intersection(vowels_space_punct)
     sleep(1)
-    chrome_driver.find_element(By.XPATH, "//button[contains(.,\'Оставить ещё и .,-!?\')]").click()
-    chrome_driver.find_element(By.XPATH, "//button[contains(.,\'Выделить результат\')]").click()
+    BUTTON_PUNCTUATION.click()
+    BUTTON_HIGHLIGHT.click()
     sleep(1)
     output_text = chrome_driver.find_element('id', 'text_output').text
     output_text = output_text.replace('\n', '')
@@ -79,18 +106,23 @@ def test_vowels_space_punctuation_with_default_text():
 
 
 def test_vowels_in_random_input_text():
-    '''This test checks if on page present buttons which filtrate in random text only vowels'''
+    """TEST.
+
+    This test checks if on page present buttons which
+    filtrate in random text only vowels.
+    """
     chrome_driver.find_element('name', 'text_input').clear()
     chrome_driver.find_element \
         ('name', 'text_input').send_keys(
-        'На золотом крыльце сидели,\nЦарь-царевич, Король-королевич,'
-        '\nСапожник, портной. Кто ты будешь такой?\n'
-        'Выбирай поскорей!\nНе задерживай добрых и честных людей!'
-        '\n\nФёдор Михайлович Достоевский.')
+        'На золотом крыльце сидели,\nЦарь-царевич, '
+        'Король-королевич,\nСапожник, портной. '
+        'Кто ты будешь такой?\nВыбирай поскорей!\n'
+        'Не задерживай добрых и честных людей!'
+        '\n\nФёдор Михайлович Достоевский.'
+    )
     match_input = {'ё', 'а', 'е', 'ю', 'о', 'у', 'ы', 'и'}
-    chrome_driver.find_element \
-        (By.XPATH, "//button[contains(.,\'Оставить только гласные\')]").click()
-    chrome_driver.find_element(By.XPATH, "//button[contains(.,\'Выделить результат\')]").click()
+    BUTTON_VOWELS.click()
+    BUTTON_HIGHLIGHT.click()
     sleep(1)
     output_text = chrome_driver.find_element('id', 'text_output').text
     set_output = convert_text_into_set(output_text)
@@ -99,12 +131,15 @@ def test_vowels_in_random_input_text():
 
 
 def test_vowels_space_in_random_input_text():
-    '''This test checks if on page present buttons which filtrate in random text only
-        vowels and spaces'''
+    """TEST.
+
+    This test checks if on page present buttons which
+    filtrate in random text only vowels and spaces.
+    """
     vowels_space = VOWELS.union(SPACE)
     match_input = {'ё', 'а', 'е', 'ю', 'о', 'у', 'ы', 'и', ' '}
-    chrome_driver.find_element(By.XPATH, "//button[contains(.,\'Ну и ещё пробелы\')]").click()
-    chrome_driver.find_element(By.XPATH, "//button[contains(.,\'Выделить результат\')]").click()
+    BUTTON_SPACES.click()
+    BUTTON_HIGHLIGHT.click()
     sleep(1)
     output_text = chrome_driver.find_element('id', 'text_output').text
     set_output = convert_text_into_set(output_text)
@@ -113,12 +148,18 @@ def test_vowels_space_in_random_input_text():
 
 
 def test_vowels_space_punct_in_random_input_text():
-    '''This test checks if on page present buttons which filtrate in random text only
-        vowels, spaces and punctuation'''
+    """TEST.
+
+    This test checks if on page present buttons which
+    filtrate in random text only vowels, spaces and punctuation.
+    """
     vowels_space_punct = (VOWELS.union(SPACE)).union(PUNTCUATION)
-    match_input = {'ё', 'а', 'е', 'ю', 'о', 'у', 'ы', 'и', ' ', ',', '.', '?', '!', '-'}
-    chrome_driver.find_element(By.XPATH, "//button[contains(.,\'Оставить ещё и .,-!?\')]").click()
-    chrome_driver.find_element(By.XPATH, "//button[contains(.,\'Выделить результат\')]").click()
+    match_input = {
+        'ё', 'а', 'е', 'ю', 'о', 'у', 'ы', 'и',
+        ' ', ',', '.', '?', '!', '-',
+    }
+    BUTTON_PUNCTUATION.click()
+    BUTTON_HIGHLIGHT.click()
     sleep(1)
     output_text = chrome_driver.find_element('id', 'text_output').text
     set_output = convert_text_into_set(output_text)
@@ -127,25 +168,85 @@ def test_vowels_space_punct_in_random_input_text():
 
 
 def test_blank_input_text():
-    '''Tests for checking blank fild in input text and using all buttons'''
-    chrome_driver.find_element \
-        ('name', 'text_input').clear()
-    chrome_driver.find_element \
-        (By.XPATH, "//button[contains(.,\'Оставить только гласные\')]").click()
-    chrome_driver.find_element(By.XPATH, "//button[contains(.,\'Выделить результат\')]").click()
+    """Tests for checking blank fild in input text and using all buttons."""
+    chrome_driver.find_element('name', 'text_input').clear()
+    BUTTON_VOWELS.click()
+    BUTTON_HIGHLIGHT.click()
     sleep(1)
     result = chrome_driver.find_element('id', 'text_output').text
     assert result == ''
-    chrome_driver.find_element(By.XPATH, "//button[contains(.,\'Ну и ещё пробелы\')]").click()
-    chrome_driver.find_element(By.XPATH, "//button[contains(.,\'Выделить результат\')]").click()
+    BUTTON_SPACES.click()
+    BUTTON_HIGHLIGHT.click()
     sleep(1)
     result = chrome_driver.find_element('id', 'text_output').text
     assert result == ''
-    chrome_driver.find_element(By.XPATH, "//button[contains(.,\'Оставить ещё и .,-!?\')]").click()
-    chrome_driver.find_element(By.XPATH, "//button[contains(.,\'Выделить результат\')]").click()
+    BUTTON_PUNCTUATION.click()
+    BUTTON_HIGHLIGHT.click()
     sleep(1)
     result = chrome_driver.find_element('id', 'text_output').text
     assert result == ''
+
+
+def test_check_button_location_on_max_window():
+    """Check location of buttons with maximum windows size."""
+    find_vowels_button_locatioin = BUTTON_VOWELS
+    location_vowels_button = find_vowels_button_locatioin.location
+    vowels_vertical = location_vowels_button['y']
+    find_and_spaces_location = BUTTON_SPACES
+    location_and_spaces_button = find_and_spaces_location.location
+    and_spaces_vertical = location_and_spaces_button['y']
+    find_so_punctuation_button = BUTTON_PUNCTUATION
+    location_so_punctuation_button = find_so_punctuation_button.location
+    so_punctuation_vertical = location_so_punctuation_button['y']
+    find_highlight_results_button = BUTTON_HIGHLIGHT
+    location_highlight_results_button = find_highlight_results_button.location
+    highlight_results_vertical = location_highlight_results_button['y']
+    assert vowels_vertical == and_spaces_vertical
+    assert and_spaces_vertical == so_punctuation_vertical
+    assert so_punctuation_vertical == highlight_results_vertical
+
+
+def test_check_button_location_mid_size_window():
+    """Check buttons location with middle window size."""
+    chrome_driver.set_window_size(500, 800)
+    find_vowels_button_locatioin = BUTTON_VOWELS
+    location_vowels_button = find_vowels_button_locatioin.location
+    vowels_vertical = location_vowels_button['y']
+    find_and_spaces_location = BUTTON_SPACES
+    location_and_spaces_button = find_and_spaces_location.location
+    and_spaces_vertical = location_and_spaces_button['y']
+    find_so_punctuation_button = BUTTON_PUNCTUATION
+    location_so_punctuation_button = find_so_punctuation_button.location
+    so_punctuation_vertical = location_so_punctuation_button['y']
+    find_highlight_results_button = BUTTON_HIGHLIGHT
+    location_highlight_results_button = find_highlight_results_button.location
+    highlight_results_vertical = location_highlight_results_button['y']
+    assert vowels_vertical <= and_spaces_vertical
+    assert and_spaces_vertical == so_punctuation_vertical
+    assert so_punctuation_vertical < highlight_results_vertical
+
+
+def test_check_button_location_min_size_window():
+    """Check button location with minimal window size."""
+    chrome_driver.set_window_size(400, 800)
+    sleep(1)
+    pyautogui.press('F12')
+    sleep(3)
+    find_vowels_button_locatioin = BUTTON_VOWELS
+    location_vowels_button = find_vowels_button_locatioin.location
+    vowels_vertical = location_vowels_button['y']
+    find_and_spaces_location = BUTTON_SPACES
+    location_and_spaces_button = find_and_spaces_location.location
+    and_spaces_vertical = location_and_spaces_button['y']
+    find_so_punctuation_button = BUTTON_PUNCTUATION
+    location_so_punctuation_button = find_so_punctuation_button.location
+    so_punctuation_vertical = location_so_punctuation_button['y']
+    find_highlight_results_button = BUTTON_HIGHLIGHT
+    location_highlight_results_button = find_highlight_results_button.location
+    highlight_results_vertical = location_highlight_results_button['y']
+    assert vowels_vertical < and_spaces_vertical
+    assert and_spaces_vertical < so_punctuation_vertical
+    assert so_punctuation_vertical < highlight_results_vertical
 
 
     chrome_driver.close()
